9a630d5cb18629a67790e3725782e9d717bcc6e1
Badry Youness <younessbadry2@gmail.com>
2022-01-10 12:47:29 +0100

    forst commit

---
+++ src/views/Login.vue
@@ -0,0 +1,55 @@
+<template>
+
+
+
+      <form>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <input type="email" class="form-control" placeholder="Email">
+        <input type="password" class="form-control" placeholder="Password">
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+      </form>
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style>
+
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="submit">
+    <h1 class="h3 mb-3 fw-normal">Please register</h1>
+    <input v-model="userInfos.name" class="form-control" placeholder="Name">
+    <input v-model="userInfos.email" type="email" class="form-control" placeholder="Email">
+    <input v-model="userInfos.password" type="password" class="form-control" placeholder="Password">
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Submit</button>
+    <p>{{userInfos.name}}</p>
+  </form>
+</template>
+
+<script>
+
+export default {
+  name: "Register",
+  data() {
+    return {
+      userInfos: {
+        name: "",
+        email: "",
+        password: "",
+      },
+
+    }
+  },
+  methods: {
+    submit() {
+      console.log(this.userInfos);
+    },
+
+  }
+
+
+
+};
+</script>
+
